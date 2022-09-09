2e54a5339d52c43cc80b26b499a01428c88d728d
Dmitriifd <idimka9@gmail.com>
2022-09-09 14:12:51 +0500

    задача reject

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
-const arr = [{ id: 1 }, { id: 2 }, { id: 3 }];
+const users = [
+	{
+		id: 1,
+		name: 'John',
+		isActive: true,
+		likes: 110,
+	},
+	{
+		id: 2,
+		name: 'Mike',
+		isActive: false,
+		likes: 30,
+	},
+];
 
-const res = _.reject(arr, (elem) => elem.id === 1);
-console.log(res);
+// Функция должна вернуть массив пользователей которые популярны. isActive === true, likes >= 100
+
+const getPopularUsers = (users) => {
+	return _.reject(users, (user) => !user.isActive || user.likes < 100);
+};
+
+// вариант с filter
+const getPopularUsers2 = (users) => {
+	return _.filter(users, (user) => user.isActive && user.likes > 100);
+};
+
+const result = getPopularUsers(users);
+console.log(getPopularUsers(users));
