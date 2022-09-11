9cc62e3484b531645db438f0c46cf5d81c18ba51
Dmitriifd <idimka9@gmail.com>
2022-09-11 12:48:44 +0500

    регистры toLower toUpper

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const numbers = [1,2,3];
+const str = 'string'
+
+_.toLower(str)
+_.toUpper(str)
+
+const res = _.chain('FOO').toLower().split('').value()
+
+console.log(_.toUpper(str))
+console.log(res);
