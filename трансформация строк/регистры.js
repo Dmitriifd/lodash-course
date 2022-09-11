f621aa621e987e135e59535187e1fe382936b699
Dmitriifd <idimka9@gmail.com>
2022-09-11 13:26:29 +0500

    регистры camelCase & snakeCase

--- трансформация строк/регистры.js
+++ трансформация строк/регистры.js
@@ -7,3 +7,7 @@ const res = _.chain('FOO').toLower().split('').value()
 
 console.log(_.toUpper(str))
 console.log(res)
+
+_.camelCase('Foo bar baz') // fooBarBaz
+_.snakeCase('Foo bar baz') // foo_bar_baz
+_.snakeCase('productName') // product_name
