032a5d9f08f2491cad7e9377ddc675cb39bbb731
Dmitriifd <idimka9@gmail.com>
2022-09-12 19:31:34 +0500

    debounce

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+const input = document.getElementById('name')
 
+const handler = function (e) {
+	console.log('input', e.target.value)
+}
+
+input.addEventListener('keydown', _.debounce(handler, 2000))
---
+++ продвинутые функции/debounce.js
@@ -0,0 +1,2 @@
+_.debounce(func, [(wait = 0)], [(options = {})]) // Аргументы: функция, задержка, опции
+_.debounce(func, 250, { maxWait: 1000 })
